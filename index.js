e49c1f0472e8d7660f6f9adc1c924888677805f6
Michael <davmadden@Madden-Family-iMac.local>
2016-08-15 01:40:22 -0500

    bootstrap and jquery

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ var samplerate = 44100;
 var id = 0;
 
 app.use(express.static(__dirname + '/public'));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
